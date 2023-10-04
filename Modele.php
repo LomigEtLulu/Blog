d30acdbe415d87c669a8657e385eae0e62056fb2
LomigEtLulu <lomiglegall@gmail.com>
2023-10-04 16:09:22 +0200

    Nouvelle modification

---
+++ Modele.php
@@ -0,0 +1,5 @@
+ <?php
+        $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8','root', '');
+        $billets = $bdd->query('select idBillet as id, dateBillet as date,'
+                        . ' titreBillet as titre, contenuBillet as contenu from BILLET'
+                        . ' order by idBillet desc');
